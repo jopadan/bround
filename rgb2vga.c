f8c851b4cafa3d24323b6c50e9d9fc8486f67ae4
Jon Daniel <joneqdaniel@gmail.com>
2025-07-01 00:57:19 +0200

    use size_t for counter

--- rgb2vga.c
+++ rgb2vga.c
@@ -6,7 +6,7 @@ static inline uint8_t rgb2vga(uint8_t v8) { return ubroundf(v8*63.0f/255.0f); }
 
 int main(int argc, char** argv)
 {
-	for(uint8_t i = 0; i < 255; i++)
-		printf("%hhu\n", rgb2vga(i));
+	for(size_t i = 0; i < 256; i++)
+		printf("%hhu: %hhu\n", (uint8_t)i, rgb2vga(i));
 	exit(EXIT_SUCCESS);
 }
