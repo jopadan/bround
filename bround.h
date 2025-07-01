f0ec1fdc1e38cb721f83a4674b984d9c2798978c
Jon Daniel <joneqdaniel@gmail.com>
2025-07-01 21:55:25 +0200

    use static inline

--- bround.h
+++ bround.h
@@ -1,25 +1,25 @@
 #include <stdint.h>
 #include <float.h>
 
-uint8_t ubround(double x)
+static inline uint8_t ubround(double x)
 {
         x += (double)((1lu << (DBL_MANT_DIG - 1)) + (1lu << (DBL_MANT_DIG - 2)));
         return *(uint8_t*)&x;
 }
 
-uint8_t ubroundf(float x)
+static inline uint8_t ubroundf(float x)
 {
         x += (float)((1u   << (FLT_MANT_DIG - 1)) + (1u  << (FLT_MANT_DIG - 2)));
         return *(uint8_t*)&x;
 }
 
-int8_t bround(double x)
+static inline int8_t bround(double x)
 {
         x += (double)((1lu << (DBL_MANT_DIG - 1)) + (1lu << (DBL_MANT_DIG - 2)));
         return *(int8_t*)&x;
 }
 
-int8_t broundf(float x)
+static inline int8_t broundf(float x)
 {
         x += (float)((1u   << (FLT_MANT_DIG - 1)) + (1u  << (FLT_MANT_DIG - 2)));
         return *(int8_t*)&x;
